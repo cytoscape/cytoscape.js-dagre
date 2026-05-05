6f9ee3230ff471b6d99810a427bd9207a778519d
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2026-05-05 12:56:35 +0200

    to increase level of legal protection of authors and users of this code, proposal to include license headers in all source files.
    This also adds the year 2026 to the main LICENSE fail

--- src/layout.js
+++ src/layout.js
@@ -1,3 +1,25 @@
+/*
+copyright (c) 2016-2018, 2020, 2022, 2026 the cytoscape consortium.
+
+permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the “software”), to deal in
+the software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the software, and to permit persons to whom the software is furnished to do
+so, subject to the following conditions:
+
+the above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the software.
+
+the software is provided “as is”, without warranty of any kind, express or
+implied, including but not limited to the warranties of merchantability,
+fitness for a particular purpose and noninfringement. in no event shall the
+authors or copyright holders be liable for any claim, damages or other
+liability, whether in an action of contract, tort or otherwise, arising from,
+out of or in connection with the software or the use or other dealings in the
+software.
+*/
+
 const isFunction = function(o){ return typeof o === 'function'; };
 const defaults = require('./defaults');
 const assign = require('./assign');
